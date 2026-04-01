b159d6b78632b7c0f7b8ab0914b8ad8d7ccf6327
tracer-ics2025 <tracer@njuics.org>
2026-04-01 20:47:32 +0800

    >  compile NEMU 231220000 张三 Linux zyb-AI 6.8.0-106-generic #106~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar  6 08:44:59 UTC  x86_64 x86_64 x86_64 GNU/Linux  20:47:32 up  5:00,  1 user,  load average: 1.57, 1.73, 1.70

--- navy-apps/tests/hello/hello.c
+++ navy-apps/tests/hello/hello.c
@@ -2,8 +2,15 @@
 #include <stdio.h>
 
 int main() {
-  printf("ceshi\n");
-  write(1, "HELLO\n", 6);
-  while (1) { }
+  write(1, "Hello World!\n", 13);
+  int i = 2;
+  volatile int j = 0;
+  while (1) {
+    j ++;
+    if (j == 10000) {
+      printf("Hello World from Navy-apps for the %dth time!\n", i ++);
+      j = 0;
+    }
+  }
   return 0;
 }
