659e9a6a96a3ab14f3549067f26f808ad6c93c24
tracer-ics2025 <tracer@njuics.org>
2026-04-01 16:39:18 +0800

    >  compile NEMU 231220000 张三 Linux zyb-AI 6.8.0-106-generic #106~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar  6 08:44:59 UTC  x86_64 x86_64 x86_64 GNU/Linux  16:39:18 up 52 min,  1 user,  load average: 1.31, 1.58, 1.41

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -21,7 +21,7 @@ void init_proc() {
   // naive_uload(NULL, "/bin/timer-test");
   // naive_uload(NULL, "/bin/event-test");
   // naive_uload(NULL, "/bin/bmp-test");
-  naive_uload(NULL, "/bin/file-test");
+  naive_uload(NULL, "/bin/hello");
 }
 
 Context* schedule(Context *prev) {
