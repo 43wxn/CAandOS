bd3d0e4a29bcf2f1f73731c24af3ded8715badc8
tracer-ics2025 <tracer@njuics.org>
2026-04-01 13:16:48 +0800

    >  compile NEMU 231220000 张三 Linux zyb-AI 6.8.0-106-generic #106~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar  6 08:44:59 UTC  x86_64 x86_64 x86_64 GNU/Linux  13:16:48 up 56 min,  1 user,  load average: 1.84, 1.50, 1.47

--- navy-apps/tests/file-test/main.c
+++ navy-apps/tests/file-test/main.c
@@ -1,54 +1,30 @@
 #include <stdio.h>
 #include <assert.h>
-#include <sys/types.h>
+#include <string.h>
 
 int main() {
   FILE *fp = fopen("/share/files/num", "r+");
   assert(fp);
 
-  printf("sizeof(long)=%d sizeof(off_t)=%d\n",
-         (int)sizeof(long), (int)sizeof(off_t));
+  int ret = fseek(fp, 500 * 5, SEEK_SET);
+  printf("fseek ret = %d\n", ret);
 
-  int r1 = fseek(fp, 0, SEEK_END);
-  printf("fseek end ret = %d\n", r1);
+  char buf[33];
+  memset(buf, 0, sizeof(buf));
+  size_t nr = fread(buf, 1, 32, fp);
 
-  long t1 = ftell(fp);
-  printf("ftell after SEEK_END = %ld\n", t1);
-
-  int r2 = fseek(fp, 0, SEEK_CUR);
-  printf("fseek cur ret = %d\n", r2);
-
-  long t2 = ftell(fp);
-  printf("ftell after SEEK_CUR = %ld\n", t2);
-
-  assert(t1 == 5000);
-  assert(t2 == 5000);
-
-  fseek(fp, 500 * 5, SEEK_SET);
-  int i, n;
-  for (i = 500; i < 1000; i++) {
-    fscanf(fp, "%d", &n);
-    assert(n == i + 1);
+  printf("fread nr = %d\n", (int)nr);
+  printf("raw bytes:\n");
+  for (int i = 0; i < (int)nr; i++) {
+    char c = buf[i];
+    if (c == '\n') printf("\\n");
+    else putchar(c);
   }
+  putchar('\n');
 
-  fseek(fp, 0, SEEK_SET);
-  for (i = 0; i < 500; i++) {
-    fprintf(fp, "%4d\n", i + 1 + 1000);
-  }
+  int n = -1;
+  ret = sscanf(buf, "%d", &n);
+  printf("sscanf ret = %d, n = %d\n", ret, n);
 
-  for (i = 500; i < 1000; i++) {
-    fscanf(fp, "%d", &n);
-    assert(n == i + 1);
-  }
-
-  fseek(fp, 0, SEEK_SET);
-  for (i = 0; i < 500; i++) {
-    fscanf(fp, "%d", &n);
-    assert(n == i + 1 + 1000);
-  }
-
-  fclose(fp);
-
-  printf("PASS!!!\n");
   return 0;
 }
