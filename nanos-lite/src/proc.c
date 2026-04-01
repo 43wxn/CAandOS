51762629e2c0f6793988406623509b3d08010cc5
tracer-ics2025 <tracer@njuics.org>
2026-04-01 21:01:01 +0800

    >  compile NEMU 231220000 张三 Linux zyb-AI 6.8.0-106-generic #106~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar  6 08:44:59 UTC  x86_64 x86_64 x86_64 GNU/Linux  21:01:01 up  5:14,  1 user,  load average: 1.11, 1.29, 1.43

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -15,7 +15,7 @@ void switch_boot_pcb() {
 void init_proc() {
   switch_boot_pcb();
   Log("Initializing processes...");
-  naive_uload(NULL, "/bin/timer-test");
+  naive_uload(NULL, "/bin/file-test");
 }
 
 Context* schedule(Context *prev) {
