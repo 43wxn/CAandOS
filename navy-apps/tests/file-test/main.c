cd243a03199c9d39693df959ac30a6809dc7ba7c
tracer-ics2025 <tracer@njuics.org>
2026-04-01 21:38:53 +0800

    >  compile NEMU 231220000 张三 Linux zyb-AI 6.8.0-106-generic #106~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar  6 08:44:59 UTC  x86_64 x86_64 x86_64 GNU/Linux  21:38:53 up  5:52,  1 user,  load average: 1.63, 1.34, 1.38

--- navy-apps/tests/file-test/main.c
+++ navy-apps/tests/file-test/main.c
@@ -1,20 +1,38 @@
+#include <stdio.h>
+#include <assert.h>
 #include <unistd.h>
-#include <fcntl.h>
-
-static void putnum(int x) {
-  char buf[32];
-  int n = 0;
-  if (x == 0) { write(1, "0\n", 2); return; }
-  if (x < 0) { write(1, "-", 1); x = -x; }
-  while (x > 0) { buf[n++] = '0' + (x % 10); x /= 10; }
-  for (int i = n - 1; i >= 0; i--) write(1, &buf[i], 1);
-  write(1, "\n", 1);
-}
 
 int main() {
-  int fd = open("/share/files/num", O_RDWR, 0666);
-  putnum(fd);        // 先立刻打印
-  write(1, "O2\n", 3);
-  putnum(fd);        // 再打印一次
-  while (1) { }
+  write(1, "M1\n", 3);
+
+  FILE *fp = fopen("/share/files/num", "r+");
+
+  write(1, "M2\n", 3);
+
+  if (!fp) {
+    write(1, "OPEN_NULL\n", 10);
+    return 1;
+  }
+
+  write(1, "M3\n", 3);
+
+  int r = fseek(fp, 0, SEEK_END);
+  if (r != 0) {
+    write(1, "FSEEK_FAIL\n", 11);
+    return 2;
+  }
+
+  write(1, "M4\n", 3);
+
+  long size = ftell(fp);
+  if (size != 5000) {
+    write(1, "FTELL_BAD\n", 10);
+    return 3;
+  }
+
+  write(1, "M5\n", 3);
+
+  fclose(fp);
+  write(1, "PASS_STAGE1\n", 12);
+  return 0;
 }
