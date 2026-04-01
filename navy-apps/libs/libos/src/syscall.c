5006326a31433987c15f139a5e59aaa7fd186ebd
tracer-ics2025 <tracer@njuics.org>
2026-04-01 20:43:33 +0800

    >  compile NEMU 231220000 张三 Linux zyb-AI 6.8.0-106-generic #106~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar  6 08:44:59 UTC  x86_64 x86_64 x86_64 GNU/Linux  20:43:33 up  4:56,  1 user,  load average: 1.58, 1.86, 1.71

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -16,23 +16,20 @@ typedef struct {
   int32_t tv_usec;
 } user_timeval_t;
 
-__attribute__((noinline))
 static intptr_t do_syscall(intptr_t type, intptr_t arg0, intptr_t arg1, intptr_t arg2) {
-  register intptr_t a0 asm("a0") = arg0;
-  register intptr_t a1 asm("a1") = arg1;
-  register intptr_t a2 asm("a2") = arg2;
-  register intptr_t a7 asm("a7") = type;
-
+  intptr_t ret;
   asm volatile (
-    "ecall"
-    : "+r"(a0)
-    : "r"(a1), "r"(a2), "r"(a7)
-    : "memory",
-      "a3", "a4", "a5", "a6",
-      "t0", "t1", "t2", "t3", "t4", "t5", "t6"
+    "mv a7, %1\n"
+    "mv a0, %2\n"
+    "mv a1, %3\n"
+    "mv a2, %4\n"
+    "ecall\n"
+    "mv %0, a0\n"
+    : "=r"(ret)
+    : "r"(type), "r"(arg0), "r"(arg1), "r"(arg2)
+    : "a0", "a1", "a2", "a7", "memory"
   );
-
-  return a0;
+  return ret;
 }
 
 intptr_t _syscall_(intptr_t type, intptr_t a0, intptr_t a1, intptr_t a2) {
