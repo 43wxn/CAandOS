485c2c3c561c3edbea80250ca21c8904586032a7
tracer-ics2025 <tracer@njuics.org>
2026-04-01 21:26:58 +0800

    >  compile NEMU 231220000 张三 Linux zyb-AI 6.8.0-106-generic #106~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar  6 08:44:59 UTC  x86_64 x86_64 x86_64 GNU/Linux  21:26:58 up  5:40,  1 user,  load average: 1.03, 1.46, 1.52

--- navy-apps/tests/file-test/main.c
+++ navy-apps/tests/file-test/main.c
@@ -4,32 +4,17 @@
 static void putnum(int x) {
   char buf[32];
   int n = 0;
-
-  if (x == 0) {
-    write(1, "0\n", 2);
-    return;
-  }
-
-  if (x < 0) {
-    write(1, "-", 1);
-    x = -x;
-  }
-
-  while (x > 0) {
-    buf[n++] = '0' + (x % 10);
-    x /= 10;
-  }
-  for (int i = n - 1; i >= 0; i--) {
-    write(1, &buf[i], 1);
-  }
+  if (x == 0) { write(1, "0\n", 2); return; }
+  if (x < 0) { write(1, "-", 1); x = -x; }
+  while (x > 0) { buf[n++] = '0' + (x % 10); x /= 10; }
+  for (int i = n - 1; i >= 0; i--) write(1, &buf[i], 1);
   write(1, "\n", 1);
 }
 
 int main() {
-  write(1, "O1\n", 3);
   int fd = open("/share/files/num", O_RDWR, 0666);
+  putnum(fd);        // 先立刻打印
   write(1, "O2\n", 3);
-  putnum(fd);
+  putnum(fd);        // 再打印一次
   while (1) { }
-  return 0;
 }
