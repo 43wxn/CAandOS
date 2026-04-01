e7e7d1f053893d639997a4f1fe3cd8ce0da5361c
tracer-ics2025 <tracer@njuics.org>
2026-04-01 13:20:18 +0800

    >  compile NEMU 231220000 张三 Linux zyb-AI 6.8.0-106-generic #106~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar  6 08:44:59 UTC  x86_64 x86_64 x86_64 GNU/Linux  13:20:18 up 59 min,  1 user,  load average: 2.43, 2.08, 1.71

--- navy-apps/tests/file-test/main.c
+++ navy-apps/tests/file-test/main.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 #include <assert.h>
 #include <string.h>
+#include <errno.h>
+#include <unistd.h>
 
 int main() {
   FILE *fp = fopen("/share/files/num", "r+");
   assert(fp);
+
+  int fd = fileno(fp);
+  printf("fileno = %d\n", fd);
 
   int ret = fseek(fp, 500 * 5, SEEK_SET);
   printf("fseek ret = %d\n", ret);
@@ -13,18 +18,32 @@ int main() {
   memset(buf, 0, sizeof(buf));
   size_t nr = fread(buf, 1, 32, fp);
 
-  printf("fread nr = %d\n", (int)nr);
-  printf("raw bytes:\n");
+  printf("after fread: nr=%d feof=%d ferror=%d errno=%d\n",
+         (int)nr, feof(fp), ferror(fp), errno);
+
+  printf("buf from fread: [");
   for (int i = 0; i < (int)nr; i++) {
     char c = buf[i];
     if (c == '\n') printf("\\n");
     else putchar(c);
   }
-  putchar('\n');
+  printf("]\n");
 
-  int n = -1;
-  ret = sscanf(buf, "%d", &n);
-  printf("sscanf ret = %d, n = %d\n", ret, n);
+  // 再直接走低级接口验证一次
+  ret = lseek(fd, 500 * 5, SEEK_SET);
+  printf("lseek(fd) ret = %d\n", ret);
+
+  memset(buf, 0, sizeof(buf));
+  int nread = read(fd, buf, 32);
+  printf("after read: nread=%d errno=%d\n", nread, errno);
+
+  printf("buf from read: [");
+  for (int i = 0; i < nread; i++) {
+    char c = buf[i];
+    if (c == '\n') printf("\\n");
+    else putchar(c);
+  }
+  printf("]\n");
 
   return 0;
 }
