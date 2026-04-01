2cff5391c4323486706e9889608d4e7466ab770b
tracer-ics2025 <tracer@njuics.org>
2026-04-01 19:36:52 +0800

    >  compile NEMU 231220000 张三 Linux zyb-AI 6.8.0-106-generic #106~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar  6 08:44:59 UTC  x86_64 x86_64 x86_64 GNU/Linux  19:36:52 up  3:50,  1 user,  load average: 1.28, 1.51, 1.50

--- navy-apps/tests/hello/hello.c
+++ navy-apps/tests/hello/hello.c
@@ -1,9 +1,11 @@
 #include <unistd.h>
-
+#include <stdio.h>
 int main() {
+  printf("ceshi\n");
   write(1, "HELLO_MARKER_20260401\n", 22);
   int i = 2;
   volatile int j = 0;
+  write(1, "LOOP\n", 5);
   while (1) {
     j ++;
     if (j == 10000) {
