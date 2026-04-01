bdd1433666fcf3e0dd9bfbe3f01645849d1af5ee
tracer-ics2025 <tracer@njuics.org>
2026-04-01 16:06:39 +0800

    >  compile NEMU 231220000 张三 Linux zyb-AI 6.8.0-106-generic #106~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar  6 08:44:59 UTC  x86_64 x86_64 x86_64 GNU/Linux  16:06:39 up 19 min,  1 user,  load average: 1.07, 1.33, 1.07

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -7,10 +7,10 @@
 
 void do_syscall(Context *c) {
   uintptr_t a[4];
-  a[0] = c->GPR1;  // syscall no
-  a[1] = c->GPR2;  // arg0
-  a[2] = c->GPR3;  // arg1
-  a[3] = c->GPR4;  // arg2
+  a[0] = c->GPR1;
+  a[1] = c->GPR2;
+  a[2] = c->GPR3;
+  a[3] = c->GPR4;
 
   switch (a[0]) {
     case SYS_exit:
@@ -19,64 +19,48 @@ void do_syscall(Context *c) {
       break;
 
     case SYS_yield:
-      Log("SYS_yield()");
       c->GPRx = 0;
       yield();
       break;
 
     case SYS_open:
-      Log("SYS_open(path=%p:\"%s\", flags=%d, mode=%d)",
-          (void *)a[1], (char *)a[1], (int)a[2], (int)a[3]);
       c->GPRx = fs_open((const char *)a[1], a[2], a[3]);
-      Log("SYS_open -> %d", (int)c->GPRx);
       break;
 
     case SYS_read:
-      Log("SYS_read(fd=%d, buf=%p, len=%d)", (int)a[1], (void *)a[2], (int)a[3]);
       c->GPRx = fs_read(a[1], (void *)a[2], a[3]);
-      Log("SYS_read -> %d", (int)c->GPRx);
       break;
 
     case SYS_write:
-      Log("SYS_write(fd=%d, buf=%p, len=%d)", (int)a[1], (void *)a[2], (int)a[3]);
       c->GPRx = fs_write(a[1], (const void *)a[2], a[3]);
-      Log("SYS_write -> %d", (int)c->GPRx);
       break;
 
     case SYS_close:
-      Log("SYS_close(fd=%d)", (int)a[1]);
       c->GPRx = fs_close(a[1]);
-      Log("SYS_close -> %d", (int)c->GPRx);
       break;
 
     case SYS_lseek:
-      Log("SYS_lseek(fd=%d, offset=%d, whence=%d)", (int)a[1], (int)a[2], (int)a[3]);
       c->GPRx = fs_lseek(a[1], (off_t)a[2], a[3]);
-      Log("SYS_lseek -> %d", (int)c->GPRx);
       break;
 
     case SYS_fstat:
-      Log("SYS_fstat(fd=%d, buf=%p)", (int)a[1], (void *)a[2]);
       c->GPRx = fs_fstat(a[1], (struct stat *)a[2]);
-      Log("SYS_fstat -> %d", (int)c->GPRx);
       break;
 
     case SYS_brk:
-      Log("SYS_brk(brk=%p)", (void *)a[1]);
       c->GPRx = 0;
       break;
 
     case SYS_gettimeofday: {
-      Log("SYS_gettimeofday(tv=%p, tz=%p)", (void *)a[1], (void *)a[2]);
       struct timeval *tv = (struct timeval *)a[1];
       struct timezone *tz = (struct timezone *)a[2];
       AM_TIMER_UPTIME_T uptime = io_read(AM_TIMER_UPTIME);
 
-      if (tv != NULL) {
-        tv->tv_sec  = uptime.us / 1000000;
+      if (tv) {
+        tv->tv_sec = uptime.us / 1000000;
         tv->tv_usec = uptime.us % 1000000;
       }
-      if (tz != NULL) {
+      if (tz) {
         tz->tz_minuteswest = 0;
         tz->tz_dsttime = 0;
       }
@@ -84,22 +68,20 @@ void do_syscall(Context *c) {
       break;
     }
 
-    // 这些在 PA3 里通常不用真做，先给 ENOSYS 风格返回
-    case SYS_kill:
-    case SYS_getpid:
     case SYS_time:
     case SYS_signal:
+    case SYS_times:
+    case SYS_kill:
+    case SYS_getpid:
     case SYS_execve:
     case SYS_fork:
     case SYS_link:
     case SYS_unlink:
     case SYS_wait:
-    case SYS_times:
-      Log("SYS_%d not implemented", (int)a[0]);
       c->GPRx = -1;
       break;
 
     default:
-      panic("Unhandled syscall ID = %d", (int)a[0]);
+      panic("Unhandled syscall ID = %d", a[0]);
   }
 }
